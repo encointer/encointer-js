e8300ebaf28b11d583b03ff1eb8d44a4cc6646c2
Christian Langenbacher <christian.langenbacher91@gmail.com>
2021-03-03 10:49:25 +0100

    migrated worker tests from encointer-wallet-app

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,17 @@ const config = require('@polkadot/dev/config/jest.cjs');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
+    '@encointer/node-api(.*)$': '<rootDir>/packages/node-api/src/$1',
     '@encointer/worker-api(.*)$': '<rootDir>/packages/worker-api/src/$1',
-    '@encointer/util(.*)$': '<rootDir>/packages/util/src/$1'
+    '@encointer/util(.*)$': '<rootDir>/packages/util/src/$1',
+    '@encointer/types(.*)$': '<rootDir>/packages/types/src/$1'
   },
   modulePathIgnorePatterns: [
     '<rootDir>/build',
+    '<rootDir>/packages/node-api/build',
     '<rootDir>/packages/worker-api/build',
-    '<rootDir>/packages/util/build'
+    '<rootDir>/packages/util/build',
+    '<rootDir>/packages/types/build'
   ],
   resolver: '@polkadot/dev/config/jest-resolver.cjs'
 });
---
+++ packages/worker-api/src/worker.spec.ts
@@ -0,0 +1,135 @@
+import { Keyring } from '@polkadot/api';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { localDockerNetwork } from './testUtils/networks';
+import { EncointerWorker } from './worker';
+import WS from 'websocket';
+
+const { w3cwebsocket: WebSocket } = WS;
+
+// skip it, as this requires a worker (and hence a node) to be running
+// To my knowledge jest does not have an option to run skipped tests specifically, does it?
+// Todo: add proper CI to test this too.
+describe.skip('worker', () => {
+  const network = localDockerNetwork();
+  let keyring: Keyring;
+  let worker: EncointerWorker;
+  beforeAll(async () => {
+    jest.setTimeout(90000);
+    await cryptoWaitReady();
+    keyring = new Keyring({ type: 'sr25519' });
+    const keypair = keyring.addFromUri('//Bob');
+    const json = keypair.toJson('1234');
+    keypair.lock();
+    keyring.addFromJson(json);
+
+    worker = new EncointerWorker(network.worker, {
+      keyring: keyring,
+      types: network.customTypes,
+      // @ts-ignore
+      createWebSocket: (url) => new WebSocket(url)
+    });
+  });
+
+  describe('getWorkerPubKey', () => {
+    it('should return value', async () => {
+      const result = await worker.getShieldingKey();
+      // console.log('Shielding Key', result);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('getTotalIssuance', () => {
+    it('should return value', async () => {
+      const result = await worker.getTotalIssuance(network.chosenCid);
+      // console.log('getTotalIssuance', result);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('getParticipantCount', () => {
+    it('should return default value', async () => {
+      const result = await worker.getParticipantCount(network.chosenCid);
+      expect(result).toBe(0);
+    });
+  });
+
+  describe('getMeetupCount', () => {
+    it('should return default value', async () => {
+      const result = await worker.getMeetupCount(network.chosenCid);
+      expect(result).toBe(0);
+    });
+  });
+
+  describe('getCeremonyReward', () => {
+    it('should return default value', async () => {
+      const result = await worker.getCeremonyReward(network.chosenCid);
+      expect(result).toBe(1);
+    });
+  });
+
+  describe('getLocationTolerance', () => {
+    it('should return default value', async () => {
+      const result = await worker.getLocationTolerance(network.chosenCid);
+      expect(result).toBe(1000);
+    });
+  });
+
+  describe('getTimeTolerance', () => {
+    it('should return default value', async () => {
+      const result = await worker.getTimeTolerance(network.chosenCid);
+      expect(result.toNumber()).toBe(600000);
+    });
+  });
+
+  describe('getSchedulerState', () => {
+    it('should return value', async () => {
+      const result = await worker.getSchedulerState(network.chosenCid);
+      // console.log('schedulerStateResult', result);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('getBalance', () => {
+    it('should return value', async () => {
+      const result = await worker.getBalance({ pubKey: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty', pin: '1234' }, network.chosenCid);
+      // console.log('getBalance', result);
+      expect(result).toBeDefined();
+    });
+  });
+
+  describe('getRegistration', () => {
+    it('should return default value', async () => {
+      await cryptoWaitReady();
+      const bob = keyring.addFromUri('//Bob', { name: 'Bob default' });
+      const result = await worker.getParticipantIndex(bob, network.chosenCid);
+      expect(result.toNumber()).toBe(0);
+    });
+  });
+
+  describe('getMeetupIndex', () => {
+    it('should return default value', async () => {
+      await cryptoWaitReady();
+      const bob = keyring.addFromUri('//Bob', { name: 'Bob default' });
+      const result = await worker.getMeetupIndex(bob, network.chosenCid);
+      expect(result.toNumber()).toBe(0);
+    });
+  });
+
+  describe('getAttestations', () => {
+    it('should be empty', async () => {
+      await cryptoWaitReady();
+      const bob = keyring.addFromUri('//Bob', { name: 'Bob default' });
+      const result = await worker.getAttestations(bob, network.chosenCid);
+      expect(result.toJSON()).toStrictEqual([]);
+    });
+  });
+
+  describe('getMeetupRegistry method', () => {
+    it('should be empty', async () => {
+      await cryptoWaitReady();
+      const bob = keyring.addFromUri('//Bob', { name: 'Bob default' });
+      const result = await worker.getMeetupRegistry(bob, network.chosenCid);
+      expect(result.toJSON()).toStrictEqual([]);
+    });
+  });
+});
