6d8f9b3a7630a1fed7e1766946040746201706c9
Christian Langenbacher <christian.langenbacher91@gmail.com>
2021-03-04 18:19:33 +0100

    [types/worker] Added function toAccount(accountOrPubkey: (Keyring | PubKeyPinPair)

--- packages/worker-api/src/worker.ts
+++ packages/worker-api/src/worker.ts
@@ -1,7 +1,7 @@
 import '@polkadot/types/augment';
 
 import WebSocketAsPromised from 'websocket-as-promised';
-import { options as encointerOptions} from '@encointer/node-api';
+import { options as encointerOptions } from '@encointer/node-api';
 import { TypeRegistry } from '@polkadot/types';
 import { RegistryTypes } from '@polkadot/types/types';
 import { Keyring } from '@polkadot/keyring'
@@ -13,17 +13,17 @@ import NodeRSA from 'node-rsa';
 
 import type { KeyringPair } from '@polkadot/keyring/types';
 import type { Vec, u32, u64 } from '@polkadot/types';
-import type {AccountId, Balance, Moment} from '@polkadot/types/interfaces/runtime';
+import type { AccountId, Balance, Moment } from '@polkadot/types/interfaces/runtime';
 import type {
   Attestation, BalanceEntry,
   MeetupIndexType,
-  ParticipantIndexType,
-  SchedulerState
+  ParticipantIndexType, RegisterParticipantArgs,
+  SchedulerState, TrustedCallSigned
 } from '@encointer/types';
 
-import type {IEncointerWorker, WorkerOptions, CallOptions, PubKeyPinPair} from './interface';
-import {isPubKeyPinPair, Request} from './interface';
-import {parseBalance, parseNodeRSA} from './parsers';
+import type { IEncointerWorker, WorkerOptions, CallOptions, PubKeyPinPair } from './interface';
+import { isPubKeyPinPair, Request } from './interface';
+import { parseBalance, parseNodeRSA } from './parsers';
 import { callGetter } from './getterApi';
 
 const unwrapWorkerResponse = (self: IEncointerWorker, data: string) => {
@@ -65,7 +65,7 @@ const parseGetterResponse = (self: IEncointerWorker, responseType: string, data:
         break;
     }
   } catch (err) {
-        throw new Error(`Can't parse into ${responseType}:\n${err}`);
+    throw new Error(`Can't parse into ${responseType}:\n${err}`);
   }
   return parsedData;
 }
@@ -110,11 +110,11 @@ export class EncointerWorker extends WebSocketAsPromised implements IEncointerWo
     this.#keyring = keyring;
   }
 
-  public async getShieldingKey(options: CallOptions = {} as  CallOptions): Promise<NodeRSA> {
+  public async getShieldingKey(options: CallOptions = {} as CallOptions): Promise<NodeRSA> {
     return await callGetter<NodeRSA>(this, [Request.Worker, 'PubKeyWorker', 'NodeRSA'], {}, options)
   }
 
-  public async getTotalIssuance(cid: string, options: CallOptions = {} as  CallOptions): Promise<Balance> {
+  public async getTotalIssuance(cid: string, options: CallOptions = {} as CallOptions): Promise<Balance> {
     return await callGetter<Balance>(this, [Request.PublicGetter, 'total_issuance', 'Balance'], {cid}, options)
   }
 
@@ -142,39 +142,45 @@ export class EncointerWorker extends WebSocketAsPromised implements IEncointerWo
     return await callGetter<SchedulerState>(this, [Request.PublicGetter, 'scheduler_state', 'SchedulerState'], {cid}, options)
   }
 
-  public async getBalance(account: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<BalanceEntry> {
-    if (isPubKeyPinPair(account)) {
-      account = this.unlockKeypair(account as PubKeyPinPair);
-    }
-    return await callGetter<BalanceEntry>(this, [Request.TrustedGetter, 'balance', 'BalanceEntry'], {cid, account}, options)
+  public async getBalance(accountOrPubKey: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<BalanceEntry> {
+    return await callGetter<BalanceEntry>(this, [Request.TrustedGetter, 'balance', 'BalanceEntry'], {
+      cid,
+      account: this.toAccount(accountOrPubKey)
+    }, options)
   }
 
-  public async getParticipantIndex(account: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<ParticipantIndexType> {
-    if (isPubKeyPinPair(account)) {
-      account = this.unlockKeypair(account as PubKeyPinPair);
-    }
-    return await callGetter<ParticipantIndexType>(this, [Request.TrustedGetter, 'participant_index', 'ParticipantIndexType' ], {cid, account}, options)
+  public async getParticipantIndex(accountOrPubKey: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<ParticipantIndexType> {
+    return await callGetter<ParticipantIndexType>(this, [Request.TrustedGetter, 'participant_index', 'ParticipantIndexType'], {
+      cid,
+      account: this.toAccount(accountOrPubKey)
+    }, options)
   }
 
-  public async getMeetupIndex(account: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<MeetupIndexType> {
-    if (isPubKeyPinPair(account)) {
-      account = this.unlockKeypair(account as PubKeyPinPair);
-    }
-    return await callGetter<MeetupIndexType>(this, [Request.TrustedGetter, 'meetup_index', 'MeetupIndexType'], {cid, account}, options)
+  public async getMeetupIndex(accountOrPubKey: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<MeetupIndexType> {
+    return await callGetter<MeetupIndexType>(this, [Request.TrustedGetter, 'meetup_index', 'MeetupIndexType'], {
+      cid,
+      account: this.toAccount(accountOrPubKey)
+    }, options)
   }
 
-  public async getAttestations(account: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<Vec<Attestation>> {
-    if (isPubKeyPinPair(account)) {
-      account = this.unlockKeypair(account as PubKeyPinPair);
-    }
-    return await callGetter<Vec<Attestation>>(this, [Request.TrustedGetter, 'attestations', 'Vec<Attestation>'], {cid, account}, options)
+  public async getAttestations(accountOrPubKey: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<Vec<Attestation>> {
+    return await callGetter<Vec<Attestation>>(this, [Request.TrustedGetter, 'attestations', 'Vec<Attestation>'], {
+      cid,
+      account: this.toAccount(accountOrPubKey)
+    }, options)
   }
 
-  public async getMeetupRegistry(account: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<Vec<AccountId>> {
-    if (isPubKeyPinPair(account)) {
-      account = this.unlockKeypair(account as PubKeyPinPair);
-    }
-    return await callGetter<Vec<AccountId>>(this, [Request.TrustedGetter, 'meetup_registry', 'Vec<AccountId>'], {cid, account}, options)
+  public async getMeetupRegistry(accountOrPubKey: KeyringPair | PubKeyPinPair, cid: string, options: CallOptions = {} as CallOptions): Promise<Vec<AccountId>> {
+    return await callGetter<Vec<AccountId>>(this, [Request.TrustedGetter, 'meetup_registry', 'Vec<AccountId>'], {
+      cid,
+      account: this.toAccount(accountOrPubKey)
+    }, options)
+  }
+
+  toAccount(accountOrPubKey: (KeyringPair | PubKeyPinPair)): KeyringPair {
+    return isPubKeyPinPair(accountOrPubKey) ?
+      this.unlockKeypair(accountOrPubKey as PubKeyPinPair) :
+      accountOrPubKey as KeyringPair
   }
 
   unlockKeypair(pair: PubKeyPinPair): KeyringPair {
