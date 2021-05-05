348332c84e99830c02cf5f3df596537858d2edd6
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-05 08:38:50 +0200

    typo

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-// this file needs to be called babel.config for jest to be able to resolve it
+// this file needs to be called babel.config.js for jest to be able to resolve it
 
 module.exports = {
   extends: '@polkadot/dev/config/babel-config-cjs.cjs'
