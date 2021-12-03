d3298f2c8d0dbd2a32043d44342677572bea762f
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-03 11:55:15 +0100

    [cidUtil] replace deprectated `substr` with new `substring`

--- packages/util/src/cidUtil.ts
+++ packages/util/src/cidUtil.ts
@@ -6,7 +6,7 @@ import {TypeRegistry} from "@polkadot/types";
 
 export function communityIdentifierFromString(registry: TypeRegistry, cid: String): CommunityIdentifier {
     return registry.createType('CommunityIdentifier', {
-        geo_hash: registry.createType('GeoHash', u8aToU8a(cid.substr(0,5))),
-        digest: registry.createType('CidDigest', base58Decode(cid.substr(5))),
+        geo_hash: registry.createType('GeoHash', u8aToU8a(cid.substring(0,5))),
+        digest: registry.createType('CidDigest', base58Decode(cid.substring(5))),
     });
 }
