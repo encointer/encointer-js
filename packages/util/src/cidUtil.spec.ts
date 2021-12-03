545e47fc9d8ba78f798699a5dee075b9ea365127
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-03 12:04:40 +0100

    [utils] use `encointerOptions` in cidUtils test.

--- packages/util/src/cidUtil.spec.ts
+++ packages/util/src/cidUtil.spec.ts
@@ -2,6 +2,8 @@
 
 import {communityIdentifierFromString} from ".";
 import {TypeRegistry} from "@polkadot/types";
+import {options as encointerOptions} from "@encointer/node-api";
+import {RegistryTypes} from "@polkadot/types/types";
 
 describe('encodeFloatToFixPoint', () => {
     it('should encode integer to fixPoint', () => {
@@ -11,14 +13,7 @@ describe('encodeFloatToFixPoint', () => {
         const cid = new Uint8Array([103, 98, 115, 117, 118, 255, 255, 255, 255]);
 
         const registry = new TypeRegistry()
-        registry.register({
-            CommunityIdentifier: {
-                geo_hash: 'GeoHash',
-                digest: 'CidDigest'
-            },
-            GeoHash: '[u8; 5]',
-            CidDigest: '[u8; 4]',
-        })
+        registry.register(encointerOptions().types as RegistryTypes)
 
         expect(
             communityIdentifierFromString(registry, cidStr).toU8a(),
