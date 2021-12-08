68bcf9fdd55d2bf9ac55a1c822b9d680a668cfc7
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-08 16:24:50 +0100

    [types] add `communities.getLocations` rpc

--- packages/types/src/interfaces/community/definitions.ts
+++ packages/types/src/interfaces/community/definitions.ts
@@ -2,7 +2,7 @@ export default {
   rpc: {
     communities: {
       getAll: {
-        description: 'Get the name of all communities as Vec<CidNames>',
+        description: 'Get the cid and name of all communities as Vec<CidNames>',
         params: [
           {
             name: 'at',
@@ -11,6 +11,22 @@ export default {
           }
         ],
         type: 'Vec<CidName>'
+      },
+      getLocations: {
+        description: 'Get all registered locations of a community',
+        params: [
+          {
+            name: 'cid',
+            type: 'CommunityIdentifier',
+            isOptional: false
+          },
+          {
+            name: 'at',
+            type: 'Hash',
+            isOptional: true
+          }
+        ],
+        type: 'Vec<Location>'
       }
     }
   },
