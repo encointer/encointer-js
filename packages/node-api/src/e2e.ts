1f8a1463221a8aefe415c0fddfd4aa0de706a9b9
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-08 16:25:14 +0100

    [node-api] more e2e tests

--- packages/node-api/src/e2e.ts
+++ packages/node-api/src/e2e.ts
@@ -30,41 +30,56 @@ describe('node-api', () => {
     });
 
     describe('rpc', () => {
-        it('communities.GetAll should return empty vec', async () => {
-            const result = await api.rpc.communities.getAll();
-            // console.log(result);
-            expect(result.length).toBe(0);
-        });
-
-        it('communities.GetBusinesses should return empty vec', async () => {
-            const cid = api.createType('CommunityIdentifier', {
-                geohash: [0x00,0x00,0x00,0x00,0x00],
-                digest: [0x00,0x00,0x00,0x00,],
+        // These tests predominantly verify that we have correct rpc/type definitions
+        describe('communities', () => {
+            it('communities.GetAll should return empty vec', async () => {
+                const result = await api.rpc.communities.getAll();
+                // console.log(result);
+                expect(result.length).toBe(0);
             });
 
-            const result = await api.rpc.bazaar.getBusinesses(cid.toHex());
-            console.log(result);
-            expect(result.length).toBe(0);
+            it('communities.getLocations should return error on unknown community', async () => {
+                let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G")
+
+                try {
+                    await api.rpc.communities.getLocations(cid)
+                } catch(e) {
+                    expect(e.toString()).toBe("Error: 3: Offchain storage not found: Key [99, 105, 100, 115, 0, 0, 0, 0, 0, 255, 255, 255, 255]");
+                }
+
+            });
         });
 
-        it('communities.GetOfferings should return empty vec', async () => {
-            // random cid
-            let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G")
-            const result = await api.rpc.bazaar.getOfferings(cid);
-            // console.log(result);
-            expect(result.length).toBe(0);
+        describe('bazaar', () => {
+            it('bazaar.GetBusinesses should return empty vec', async () => {
+                const cid = api.createType('CommunityIdentifier', {
+                    geohash: [0x00, 0x00, 0x00, 0x00, 0x00],
+                    digest: [0x00, 0x00, 0x00, 0x00,],
+                });
+
+                const result = await api.rpc.bazaar.getBusinesses(cid.toHex());
+                // console.log(result);
+                expect(result.length).toBe(0);
+            });
+
+            it('bazaar.GetOfferings should return empty vec', async () => {
+                // random cid
+                let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G")
+                const result = await api.rpc.bazaar.getOfferings(cid);
+                // console.log(result);
+                expect(result.length).toBe(0);
+            });
+
+            it('bazaar.GetOfferingsForBusiness should return empty vec', async () => {
+                // random cid
+                let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G")
+                const alice = keyring.addFromUri('//Alice', {name: 'Alice default'})
+
+                const bid = api.createType('BusinessIdentifier', [cid, alice.publicKey]);
+                const result = await api.rpc.bazaar.getOfferingsForBusiness(bid);
+                // console.log(result);
+                expect(result.length).toBe(0);
+            });
         });
-
-        it('communities.GetOfferingsForBusiness should return empty vec', async () => {
-            // random cid
-            let cid = communityIdentifierFromString(api.registry, "gbsuv7YXq9G")
-            const alice = keyring.addFromUri('//Alice', { name: 'Alice default' })
-
-            const bid= api.createType('BusinessIdentifier', [cid, alice.publicKey]);
-            const result = await api.rpc.bazaar.getOfferingsForBusiness(bid);
-            // console.log(result);
-            expect(result.length).toBe(0);
-        });
-
     });
 });
