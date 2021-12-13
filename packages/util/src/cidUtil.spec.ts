39548b4fa8ad5d4f150a7de99aa2a1d1a446aa87
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-13 13:13:02 +0100

    [util] add `CommunityIdentifierToString`

--- packages/util/src/cidUtil.spec.ts
+++ packages/util/src/cidUtil.spec.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import {communityIdentifierFromString} from ".";
+import {communityIdentifierFromString, communityIdentifierToString} from ".";
 import {TypeRegistry} from "@polkadot/types";
 import {options as encointerOptions} from "@encointer/node-api";
 import {RegistryTypes} from "@polkadot/types/types";
@@ -18,5 +18,21 @@ describe('cidUtils', () => {
         expect(
             communityIdentifierFromString(registry, cidStr).toU8a(),
         ).toStrictEqual(cid);
+    });
+
+    it('should correctly format CommunityIdentifier', () => {
+        const cidStr = "gbsuv7YXq9G";
+
+        // scale-encoded `CommunityIdentifier` generated in rust.
+        const cidRaw = new Uint8Array([103, 98, 115, 117, 118, 255, 255, 255, 255]);
+
+        const registry = new TypeRegistry()
+        registry.register(encointerOptions().types as RegistryTypes)
+
+        const cid = registry.createType('CommunityIdentifier', cidRaw);
+
+        expect(
+            communityIdentifierToString(cid)
+        ).toStrictEqual(cidStr);
     });
 });
--- packages/util/src/cidUtil.ts
+++ packages/util/src/cidUtil.ts
@@ -1,6 +1,6 @@
 import {CommunityIdentifier} from "@encointer/types";
 import { u8aToU8a} from "@polkadot/util";
-import { base58Decode } from '@polkadot/util-crypto/base58';
+import { base58Decode, base58Encode } from '@polkadot/util-crypto/base58';
 import {TypeRegistry} from "@polkadot/types";
 
 /***
@@ -13,4 +13,13 @@ export function communityIdentifierFromString(registry: TypeRegistry, cid: Strin
         geohash: registry.createType('GeoHash', u8aToU8a(cid.substring(0,5))),
         digest: registry.createType('CidDigest', base58Decode(cid.substring(5))),
     });
+}
+
+/***
+ * Helper method to parse a string-formatted `CommunityIdentifier`.
+ * @param registry
+ * @param cid
+ */
+export function communityIdentifierToString(cid: CommunityIdentifier): String {
+    return cid.geohash.toUtf8() + base58Encode(cid.digest)
 }
