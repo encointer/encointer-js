aee9006c30f19a6f3d416cac41cd11e628056b29
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-16 15:46:46 +0100

    [types] add fixed point conversion wrappers for `Degree` and `EncointerBalance`

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -35,3 +35,25 @@ const encointer = {
 
 export default encointer;
 export { createType } from '@polkadot/types';
+
+import {parseI64F64, toI64F64} from "@encointer/util";
+
+/**
+ * Converts a fixed-point string representation to a fixed-point BN
+ */
+export const toEncointerBalance = toI64F64
+
+/**
+ * Parse encointer balance from a fixed-point BN to a JS number
+ */
+export const parseEncointerBalance = parseI64F64
+
+/**
+ * Converts a fixed-point string representation to a fixed-point BN
+ */
+export const toDegree = toI64F64
+
+/**
+ * Parse a fixed-point BN to a JS number
+ */
+export const parseDegree = parseI64F64
