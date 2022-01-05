7f383f883e45df3fdb27b53bb105b297bce5b554
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-01-05 16:16:06 +0100

    [utils/cidUtil] change parameter type: TypeRegistry -> Registry. This resolves some warnings in the test.

--- packages/util/src/cidUtil.ts
+++ packages/util/src/cidUtil.ts
@@ -1,14 +1,14 @@
 import {CommunityIdentifier} from "@encointer/types";
 import { u8aToU8a} from "@polkadot/util";
 import { base58Decode, base58Encode } from '@polkadot/util-crypto/base58';
-import {TypeRegistry} from "@polkadot/types";
+import {Registry} from "@polkadot/types/types";
 
 /***
  * Helper method to parse a string-formatted `CommunityIdentifier`.
  * @param registry
  * @param cid
  */
-export function communityIdentifierFromString(registry: TypeRegistry, cid: String): CommunityIdentifier {
+export function communityIdentifierFromString(registry: Registry, cid: String): CommunityIdentifier {
     return registry.createType('CommunityIdentifier', {
         geohash: registry.createType('GeoHash', u8aToU8a(cid.substring(0,5))),
         digest: registry.createType('CidDigest', base58Decode(cid.substring(5))),
