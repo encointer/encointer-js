c956b102e84b0f4a5eb6d7760e118f9e560c94aa
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-03-15 15:09:48 +0100

    [test-ceremony-data] use relative paths

--- packages/util/src/test-ceremony-data/index.ts
+++ packages/util/src/test-ceremony-data/index.ts
@@ -1,3 +1,3 @@
 
-export { default as test1 } from "@encointer/util/test-ceremony-data/test1"
-export { default as test2 } from "@encointer/util/test-ceremony-data/test2"
+export { default as test1 } from "./test1"
+export { default as test2 } from "./test2"
