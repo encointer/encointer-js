fdc979fb0cf6054ba39475d9cd8ce28a87ba03a8
clangenb <37865735+clangenb@users.noreply.github.com>
2024-05-13 11:31:59 +0200

    [GA] run encointer node in docker image in CI + fix integration tests (#104)
    * [GA] run encointer node in docker image in CI
    
    * [node-api/e2e-ksm] fix test name
    
    * [node-api/e2e] apply lint
    
    * [node-api/e2e] minor registering refactoring and fix ordering of registrations
    
    * bump encointer node docker image
    
    * [node-api/e2e] give funds to charlie again
    
    * [node-api/e2e] update transfer tx to transferKeepAlive

---
+++ scripts/docker_run_encointer_node_notee.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+DOCKER_TAG=${1:-1.8.3}
+
+echo "Encointer node docker tag: ${DOCKER_TAG}"
+
+docker run -p 30333:30333 -p 9944:9944 -p 9933:9933 -p 9615:9615 \
+  encointer/encointer-node-notee:${DOCKER_TAG} \
+  --dev \
+  --enable-offchain-indexing true \
+  --rpc-methods unsafe \
+  -lencointer=debug,parity_ws=warn \
+  --rpc-external \
