4753f34ac50e0304f85223f0147ecee442890a7b
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-10-24 22:47:00 +0200

    Revert "remove rpc provider again"
    This reverts commit 1815e0cdc46ab8ae6d97f47151b03ed60713eb9f.

---
+++ packages/rpc-provider/src/ws/connect.spec.ts
@@ -0,0 +1,167 @@
+// Copyright 2017-2024 @polkadot/rpc-provider authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/// <reference types="@polkadot/dev-test/globals.d.ts" />
+
+import type { Mock } from '../mock/types.js';
+
+import { mockWs } from '../mock/mockWs.js';
+import { WsProvider } from './index.js';
+
+const TEST_WS_URL = 'ws://localhost-connect.spec.ts:9988';
+
+function sleep (ms = 100): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe('onConnect', (): void => {
+  let mocks: Mock[];
+  let provider: WsProvider | null;
+
+  beforeEach((): void => {
+    mocks = [mockWs([], TEST_WS_URL)];
+  });
+
+  afterEach(async () => {
+    if (provider) {
+      await provider.disconnect();
+      await sleep();
+
+      provider = null;
+    }
+
+    await Promise.all(mocks.map((m) => m.done()));
+    await sleep();
+  });
+
+  it('Does not connect when autoConnect is false', async () => {
+    provider = new WsProvider(TEST_WS_URL, 0);
+
+    await sleep();
+
+    expect(provider.isConnected).toBe(false);
+
+    await provider.connect();
+    await sleep();
+
+    expect(provider.isConnected).toBe(true);
+
+    await provider.disconnect();
+    await sleep();
+
+    expect(provider.isConnected).toBe(false);
+  });
+
+  it('Does connect when autoConnect is true', async () => {
+    provider = new WsProvider(TEST_WS_URL, 1);
+
+    await sleep();
+
+    expect(provider.isConnected).toBe(true);
+  });
+
+  it('Creates a new WebSocket instance by calling the connect() method', async () => {
+    provider = new WsProvider(TEST_WS_URL, false);
+
+    expect(provider.isConnected).toBe(false);
+    expect(mocks[0].server.clients().length).toBe(0);
+
+    await provider.connect();
+    await sleep();
+
+    expect(provider.isConnected).toBe(true);
+    expect(mocks[0].server.clients()).toHaveLength(1);
+  });
+
+  it('Connects to first endpoint when an array is given', async () => {
+    provider = new WsProvider([TEST_WS_URL], 1);
+
+    await sleep();
+
+    expect(provider.isConnected).toBe(true);
+    expect(mocks[0].server.clients()).toHaveLength(1);
+  });
+
+  it('Does not allow connect() on already-connected', async () => {
+    provider = new WsProvider([TEST_WS_URL], 1);
+
+    await sleep();
+
+    expect(provider.isConnected).toBe(true);
+
+    await expect(
+      provider.connect()
+    ).rejects.toThrow(/already connected/);
+  });
+
+  it('Connects to the second endpoint when the first is unreachable', async () => {
+    const endpoints: string[] = ['ws://localhost-unreachable-connect.spec.ts:9956', TEST_WS_URL];
+
+    provider = new WsProvider(endpoints, 1);
+
+    await sleep();
+
+    expect(mocks[0].server.clients()).toHaveLength(1);
+    expect(provider.isConnected).toBe(true);
+  });
+
+  it('Connects to the second endpoint when the first is dropped', async () => {
+    const endpoints: string[] = [TEST_WS_URL, 'ws://localhost-connect.spec.ts:9957'];
+
+    mocks.push(mockWs([], endpoints[1]));
+
+    provider = new WsProvider(endpoints, 1);
+
+    await sleep();
+
+    // Check that first server is connected
+    expect(mocks[0].server.clients()).toHaveLength(1);
+    expect(mocks[1].server.clients()).toHaveLength(0);
+
+    // Close connection from first server
+    mocks[0].server.clients()[0].close();
+
+    await sleep();
+
+    // Check that second server is connected
+    expect(mocks[1].server.clients()).toHaveLength(1);
+    expect(provider.isConnected).toBe(true);
+  });
+
+  it('Round-robin of endpoints on WsProvider', async () => {
+    const endpoints: string[] = [
+      TEST_WS_URL,
+      'ws://localhost-connect.spec.ts:9956',
+      'ws://localhost-connect.spec.ts:9957',
+      'ws://invalid-connect.spec.ts:9956',
+      'ws://localhost-connect.spec.ts:9958'
+    ];
+
+    mocks.push(mockWs([], endpoints[1]));
+    mocks.push(mockWs([], endpoints[2]));
+    mocks.push(mockWs([], endpoints[4]));
+
+    const mockNext = [
+      mocks[1],
+      mocks[2],
+      mocks[3],
+      mocks[0]
+    ];
+
+    provider = new WsProvider(endpoints, 1);
+
+    for (let round = 0; round < 2; round++) {
+      for (let mock = 0; mock < mocks.length; mock++) {
+        await sleep();
+
+        // Wwe are connected, the current mock has the connection and the next doesn't
+        expect(provider.isConnected).toBe(true);
+        expect(mocks[mock].server.clients()).toHaveLength(1);
+        expect(mockNext[mock].server.clients()).toHaveLength(0);
+
+        // Close connection from first server
+        mocks[mock].server.clients()[0].close();
+      }
+    }
+  });
+});
---
+++ packages/rpc-provider/src/ws/index.spec.ts
@@ -0,0 +1,92 @@
+// Copyright 2017-2024 @polkadot/rpc-provider authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/// <reference types="@polkadot/dev-test/globals.d.ts" />
+
+import type { Request } from '../mock/mockWs.js';
+import type { Mock } from '../mock/types.js';
+
+import { mockWs } from '../mock/mockWs.js';
+import { WsProvider } from './index.js';
+
+const TEST_WS_URL = 'ws://localhost-index.spec.ts:9977';
+
+let provider: WsProvider | null;
+let mock: Mock;
+
+function createWs (requests: Request[], autoConnect = 1000, headers?: Record<string, string>, timeout?: number): WsProvider {
+  mock = mockWs(requests, TEST_WS_URL);
+  provider = new WsProvider(TEST_WS_URL, autoConnect, headers, timeout);
+
+  return provider;
+}
+
+describe('Ws', (): void => {
+  afterEach(async () => {
+    if (mock) {
+      await mock.done();
+    }
+
+    if (provider) {
+      await provider.disconnect();
+      provider = null;
+    }
+  });
+
+  it('returns the connected state', (): void => {
+    expect(
+      createWs([]).isConnected
+    ).toEqual(false);
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('allows you to initialize the provider with custom headers', () => {
+    createWs([], 100, { foo: 'bar' });
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('allows you to set custom timeout value for handlers', () => {
+    const CUSTOM_TIMEOUT_S = 90;
+    const CUSTOM_TIMEOUT_MS = CUSTOM_TIMEOUT_S * 1000;
+
+    createWs([], 100, { foo: 'bar' }, CUSTOM_TIMEOUT_MS);
+  });
+});
+
+describe('Endpoint Parsing', (): void => {
+  // eslint-disable-next-line jest/expect-expect
+  it('Succeeds when WsProvider endpoint is a valid string', () => {
+    /* eslint-disable no-new */
+    new WsProvider(TEST_WS_URL, 0);
+  });
+
+  it('Throws when WsProvider endpoint is an invalid string', () => {
+    expect(
+      () => new WsProvider('http://127.0.0.1:9955', 0)
+    ).toThrow(/^Endpoint should start with /);
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('Succeeds when WsProvider endpoint is a valid array', () => {
+    const endpoints: string[] = ['ws://127.0.0.1:9955', 'wss://testnet.io:9944', 'ws://mychain.com:9933'];
+
+    /* eslint-disable no-new */
+    new WsProvider(endpoints, 0);
+  });
+
+  it('Throws when WsProvider endpoint is an empty array', () => {
+    const endpoints: string[] = [];
+
+    expect(
+      () => new WsProvider(endpoints, 0)
+    ).toThrow('WsProvider requires at least one Endpoint');
+  });
+
+  it('Throws when WsProvider endpoint is an invalid array', () => {
+    const endpoints: string[] = ['ws://127.0.0.1:9955', 'http://bad.co:9944', 'ws://mychain.com:9933'];
+
+    expect(
+      () => new WsProvider(endpoints, 0)
+    ).toThrow(/^Endpoint should start with /);
+  });
+});
