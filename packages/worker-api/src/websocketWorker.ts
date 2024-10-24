4cfceec2b0d0e03bc21b870954d93ba90ff6c8bd
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-10-24 22:39:40 +0200

    simplify the websocket worker some more

--- packages/worker-api/src/websocketWorker.ts
+++ packages/worker-api/src/websocketWorker.ts
@@ -1,7 +1,6 @@
 import type {Vec} from '@polkadot/types';
 import {TypeRegistry} from '@polkadot/types';
 import type {RegistryTypes} from '@polkadot/types/types';
-import {Keyring} from '@polkadot/keyring'
 import {compactAddLength, hexToU8a} from '@polkadot/util';
 
 
@@ -15,88 +14,17 @@ import type {u8} from "@polkadot/types-codec";
 import BN from "bn.js";
 import {WsProvider} from "@polkadot/api";
 
-
-// const parseGetterResponse = (self: IWorker, responseType: string, data: string) => {
-//     if (data === 'Could not decode request') {
-//         throw new Error(`Worker error: ${data}`);
-//     }
-//
-//     // console.debug(`Getter response: ${data}`);
-//     const json = JSON.parse(data);
-//
-//     const value = hexToU8a(json["result"]);
-//     const returnValue = self.createType('RpcReturnValue', value);
-//     console.debug(`RpcReturnValue ${JSON.stringify(returnValue)}`);
-//
-//     if (returnValue.status.isError) {
-//         const errorMsg = self.createType('String', returnValue.value);
-//         throw new Error(`RPC Error: ${errorMsg}`);
-//     }
-//
-//     let parsedData: any;
-//     try {
-//         switch (responseType) {
-//             case 'raw':
-//                 parsedData = unwrapWorkerResponse(self, returnValue.value);
-//                 break;
-//             case 'BalanceEntry':
-//                 parsedData = unwrapWorkerResponse(self, returnValue.value);
-//                 parsedData = parseBalance(self, parsedData);
-//                 break;
-//             case 'I64F64':
-//                 parsedData = unwrapWorkerResponse(self, returnValue.value);
-//                 parsedData = parseI64F64(self.createType('i128', parsedData));
-//                 break;
-//             case 'CryptoKey':
-//                 const jsonStr = self.createType('String', returnValue.value);
-//                 // Todo: For some reason there are 2 non-utf characters, where I don't know where
-//                 // they come from currently.
-//                 console.debug(`Got shielding key: ${jsonStr.toJSON().substring(2)}`);
-//                 parsedData = parseWebCryptoRSA(jsonStr.toJSON().substring(2));
-//                 break
-//             case 'Vault':
-//                 parsedData = self.createType(responseType, returnValue.value);
-//                 break
-//             case 'EnclaveFingerprint':
-//                 parsedData = self.createType(responseType, returnValue.value);
-//                 break
-//             case 'TrustedOperationResult':
-//                 console.debug(`Got TrustedOperationResult`)
-//                 parsedData = self.createType('Hash', returnValue.value);
-//                 break
-//             default:
-//                 parsedData = unwrapWorkerResponse(self, returnValue.value);
-//                 console.debug(`unwrapped data ${parsedData}`);
-//                 parsedData = self.createType(responseType, parsedData);
-//                 break;
-//         }
-//     } catch (err) {
-//         throw new Error(`Can't parse into ${responseType}:\n${err}`);
-//     }
-//     return parsedData;
-// }
-
 export class Worker {
 
     readonly #registry: TypeRegistry;
-
-    #keyring?: Keyring;
 
     #shieldingKey?: CryptoKey;
 
     #ws: WsProvider;
 
-    rsCount: number;
-
-    rqStack: string[];
-
     constructor(url: string, options: WorkerOptions = {} as WorkerOptions) {
-        this.#keyring = (options.keyring || undefined);
         this.#registry = new TypeRegistry();
-        this.rsCount = 0;
-        this.rqStack = [] as string[]
         this.#ws = new WsProvider(url);
-
 
         if (options.types != undefined) {
             this.#registry.register(encointerOptions({types: options.types}).types as RegistryTypes);
@@ -130,14 +58,6 @@ export class Worker {
 
     public createType(apiType: string, obj?: any): any {
         return this.#registry.createType(apiType as never, obj)
-    }
-
-    public keyring(): Keyring | undefined {
-        return this.#keyring;
-    }
-
-    public setKeyring(keyring: Keyring): void {
-        this.#keyring = keyring;
     }
 
     public shieldingKey(): CryptoKey | undefined {
