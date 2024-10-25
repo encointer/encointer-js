2164072789994318c47d4c62289450a8e4f1a085
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-10-25 21:06:49 +0200

    add comment about tests and skip them again for the ci

--- packages/worker-api/src/integriteeWorker.spec.ts
+++ packages/worker-api/src/integriteeWorker.spec.ts
@@ -42,79 +42,79 @@ describe('worker', () => {
     // skip it, as this requires a worker (and hence a node) to be running
     // To my knowledge jest does not have an option to run skipped tests specifically, does it?
     // Todo: add proper CI to test this too.
-    describe('needs worker and node running', () => {
-        // describe('getWorkerPubKey', () => {
-        //     it('should return value', async () => {
-        //         const result = await worker.getShieldingKey();
-        //         // console.log('Shielding Key', result);
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('getShardVault', () => {
-        //     it('should return value', async () => {
-        //         const result = await worker.getShardVault();
-        //         console.log('ShardVault', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('getNonce', () => {
-        //     it('should return value', async () => {
-        //         const result = await worker.getNonce(alice, network.shard);
-        //         console.log('Nonce', result.toHuman);
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        //
-        // describe('getAccountInfo', () => {
-        //     it('should return value', async () => {
-        //         const result = await worker.getAccountInfo(alice, network.shard);
-        //         console.log('getAccountInfo', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('accountInfoGetter', () => {
-        //     it('should return value', async () => {
-        //         const getter = await worker.accountInfoGetter(charlie, network.shard);
-        //         console.log(`AccountInfoGetter: ${JSON.stringify(getter)}`);
-        //         const result = await getter.send();
-        //         console.log('getAccountInfo:', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('parentchainsInfoGetter', () => {
-        //     it('should return value', async () => {
-        //         const getter = worker.parentchainsInfoGetter(network.shard);
-        //         console.log(`parentchainsInfoGetter: ${JSON.stringify(getter)}`);
-        //         const result = await getter.send();
-        //         console.log('parentchainsInfoGetter:', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('guessTheNumberInfoGetter', () => {
-        //     it('should return value', async () => {
-        //         const getter = worker.guessTheNumberInfoGetter(network.shard);
-        //         console.log(`GuessTheNumberInfo: ${JSON.stringify(getter)}`);
-        //         const result = await getter.send();
-        //         console.log('GuessTheNumberInfo:', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('guessTheNumberAttemptsGetter', () => {
-        //     it('should return value', async () => {
-        //         const getter = await worker.guessTheNumberAttemptsTrustedGetter(charlie, network.shard);
-        //         console.log(`Attempts: ${JSON.stringify(getter)}`);
-        //         const result = await getter.send();
-        //         console.log('Attempts:', result.toHuman());
-        //         expect(result).toBeDefined();
-        //     });
-        // });
+    describe.skip('needs worker and node running', () => {
+        describe('getWorkerPubKey', () => {
+            it('should return value', async () => {
+                const result = await worker.getShieldingKey();
+                // console.log('Shielding Key', result);
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('getShardVault', () => {
+            it('should return value', async () => {
+                const result = await worker.getShardVault();
+                console.log('ShardVault', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('getNonce', () => {
+            it('should return value', async () => {
+                const result = await worker.getNonce(alice, network.shard);
+                console.log('Nonce', result.toHuman);
+                expect(result).toBeDefined();
+            });
+        });
+
+
+        describe('getAccountInfo', () => {
+            it('should return value', async () => {
+                const result = await worker.getAccountInfo(alice, network.shard);
+                console.log('getAccountInfo', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('accountInfoGetter', () => {
+            it('should return value', async () => {
+                const getter = await worker.accountInfoGetter(charlie, network.shard);
+                console.log(`AccountInfoGetter: ${JSON.stringify(getter)}`);
+                const result = await getter.send();
+                console.log('getAccountInfo:', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('parentchainsInfoGetter', () => {
+            it('should return value', async () => {
+                const getter = worker.parentchainsInfoGetter(network.shard);
+                console.log(`parentchainsInfoGetter: ${JSON.stringify(getter)}`);
+                const result = await getter.send();
+                console.log('parentchainsInfoGetter:', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('guessTheNumberInfoGetter', () => {
+            it('should return value', async () => {
+                const getter = worker.guessTheNumberInfoGetter(network.shard);
+                console.log(`GuessTheNumberInfo: ${JSON.stringify(getter)}`);
+                const result = await getter.send();
+                console.log('GuessTheNumberInfo:', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
+
+        describe('guessTheNumberAttemptsGetter', () => {
+            it('should return value', async () => {
+                const getter = await worker.guessTheNumberAttemptsTrustedGetter(charlie, network.shard);
+                console.log(`Attempts: ${JSON.stringify(getter)}`);
+                const result = await getter.send();
+                console.log('Attempts:', result.toHuman());
+                expect(result).toBeDefined();
+            });
+        });
 
         describe('balance transfer should work', () => {
             it('should return value', async () => {
@@ -132,36 +132,38 @@ describe('worker', () => {
             });
         });
 
-        // describe('balance unshield should work', () => {
-        //     it('should return value', async () => {
-        //         const shard = network.shard;
-        //
-        //         const result = await worker.balanceUnshieldFunds(
-        //             alice,
-        //             shard,
-        //             network.mrenclave,
-        //             alice.address,
-        //             charlie.address,
-        //             1100000000000,
-        //         );
-        //         console.log('balance unshield result', JSON.stringify(result));
-        //         expect(result).toBeDefined();
-        //     });
-        // });
-        //
-        // describe('guess the number should work', () => {
-        //     it('should return value', async () => {
-        //         const shard = network.shard;
-        //
-        //         const result = await worker.guessTheNumber(
-        //             alice,
-        //             shard,
-        //             network.mrenclave,
-        //             1,
-        //         );
-        //         console.log('guess the number result', JSON.stringify(result));
-        //         expect(result).toBeDefined();
-        //     });
-        // });
+        // race condition so skipped
+        describe.skip('balance unshield should work', () => {
+            it('should return value', async () => {
+                const shard = network.shard;
+
+                const result = await worker.balanceUnshieldFunds(
+                    alice,
+                    shard,
+                    network.mrenclave,
+                    alice.address,
+                    charlie.address,
+                    1100000000000,
+                );
+                console.log('balance unshield result', JSON.stringify(result));
+                expect(result).toBeDefined();
+            });
+        });
+
+        // race condition, so skipped
+        describe.skip('guess the number should work', () => {
+            it('should return value', async () => {
+                const shard = network.shard;
+
+                const result = await worker.guessTheNumber(
+                    alice,
+                    shard,
+                    network.mrenclave,
+                    1,
+                );
+                console.log('guess the number result', JSON.stringify(result));
+                expect(result).toBeDefined();
+            });
+        });
     });
 });
